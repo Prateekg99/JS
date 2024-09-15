99f0f1afc2c7aa86fcf8e3be774342ccad6b41f9
Prateek Kumar Gupta <108070224+Prateekg99@users.noreply.github.com>
2024-09-15 09:45:20 +0000

    oprations

--- Basics/operations.js
+++ Basics/operations.js
@@ -1,1 +1,16 @@
- 
+ console.log("2">1);
+ console.log("02">1);
+
+
+
+ // Primitive
+ // 7 types : String, Number, Boolwan, Null, undefined, Symbol, BigInt
+
+
+ //Reference type (Non primitive)
+
+ //Array, Objects, Functions  
+ let MyFunction = function () {
+    console.log("Hii this is function")
+ }
+ MyFunction();
