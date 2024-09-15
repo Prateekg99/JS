23d7fbe29c49a1b77c90cbfa9d9bd23f51954256
Prateek Kumar Gupta <108070224+Prateekg99@users.noreply.github.com>
2024-09-15 13:19:23 +0000

    string

---
+++ Basics/string.js
@@ -0,0 +1,17 @@
+ // how to write string code with "Backticks"
+
+ let name = "Prateek";
+ let age =50;
+
+//  console.log(`this is my ${name} and my fake age is ${age} `)
+
+
+const gateName = new String('Prateek-g');
+
+// console.log(gateName[6]);
+// console.log(gateName.__proto__);
+// console.log(gateName.length);
+// console.log(gateName.indexOf("r"));
+// const newString = gateName.substring(0);
+// console.log(newString);
+
