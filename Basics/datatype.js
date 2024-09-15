fc88342211c289209edbbc26d75aa76bce5b6622
Prateek Kumar Gupta <108070224+Prateekg99@users.noreply.github.com>
2024-09-15 07:07:54 +0000

    datatype

---
+++ Basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not browser
+
+let name = "Prateek";
+let age = 23;
+let isLoging =true;
+
+console.table([name,age,isLoging]);
+
+
+
+// number
+//bigint
+//boolean
+//null
+//symbol
+//undifind
+
+
+console.log(typeof age)
---
+++ Basics/var.js
@@ -0,0 +1,13 @@
+const accountId = 1414
+let accountEmail = "prateek@gmail.com";
+var accountPass = "1234"
+userCity = "BHopal;"
+
+// accountId = 5;
+console.log(accountId);
+accountEmail="ttt@gmail.com"
+accountPass="313"
+userCity="delhi"
+let accountstate;
+
+console.table([accountEmail,accountstate,accountPass,accountId,userCity])
