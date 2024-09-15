6280426b65736dd92d493c524d67fccec2ef3467
Prateek Kumar Gupta <108070224+Prateekg99@users.noreply.github.com>
2024-09-15 07:22:26 +0000

    Datatype conversion

--- Basics/datatype.js
+++ Basics/datatype.js
@@ -2,11 +2,11 @@
 
 // alert(3+3)// we are using nodejs, not browser
 
-let name = "Prateek";
-let age = 23;
-let isLoging =true;
+// let name = "Prateek";
+// let age = 23;
+// let isLoging =true;
 
-console.table([name,age,isLoging]);
+// console.table([name,age,isLoging]);
 
 
 
@@ -18,4 +18,24 @@ console.table([name,age,isLoging]);
 //undifind
 
 
-console.log(typeof age)
+// console.log(typeof age)
+
+
+
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+
+// "33" => 33
+// 33aaacx => NaN;
+
+let isLogingId =""
+let booleanIsLoggedIn =Boolean(isLogingId)
+ console.log(booleanIsLoggedIn);
